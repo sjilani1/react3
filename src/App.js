983f8ca0ef4891fba23bca93520a5eccc0c7e773
sjilani1 <syeda_merzia_jilani@brown.edu>
2024-03-17 10:15:14 -0400

    final

--- src/App.js
+++ src/App.js
@@ -11,18 +11,57 @@ bakeryData.forEach((item) => {
 function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
+  // const increment = () => setCount(count + 1)
+  // const AddtoCart() = () => setCart(cart + item)
+  const [count, setCount] = useState(0); 
+  const [ cart, setCart] =useState([]);
+
+  
+
+  const AddtoCart=(item) => { 
+    console.log(item)
+    console.log(cart)
+    setCount(count + 1)
+    setCart([...cart,item.name]);
+        
+
+      }
+
+    
+
+
+      
+    
 
   return (
     <div className="App">
       <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
 
-      {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
+      {bakeryData.map((item, index) => ( 
+        
+        <div class="card"> 
+        <div key={index}>
+          <p>
+          <h2> {item.name}</h2>
+          <p1> {item.description} </p1>
+          <p2> {item.price}</p2>
+           </p>
+           <img src={item.image}/>
+           <button onClick={ () => AddtoCart(item)}> Add to Cart</button>
+          </div> 
+          </div>
       ))}
 
       <div>
+        
         <h2>Cart</h2>
-        {/* TODO: render a list of items in the cart */}
+        
+        {count}
+        <ul>
+          {cart.map((cartItem, index) => (
+            <li key={index}>{cartItem}</li>
+          ))}
+        </ul>
       </div>
     </div>
   );
